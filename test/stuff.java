eb2d9de399c1210579028d498cd4305b4ff64d8a
trashyTrader <43192922+trashyTrader@users.noreply.github.com>
2018-11-21 19:07:09 -0800

    Create stuff.java

---
+++ test/stuff.java
@@ -0,0 +1,15 @@
+// Cameron moon / Micah Phelps / Noah Stonge
+// Project 2 sudoku solver and checker
+// 11/21/2018 CSC! 145 
+
+public class Sudoku {
+
+//============================================================================================	
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+		
+
